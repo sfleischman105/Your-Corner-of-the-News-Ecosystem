f85fbc5d55e148a1b1a350ddfcf95c854371b683
Conor Kelly <ckelly@ckellydesign.net>
2017-04-27 22:44:55 -0700

    huh...

--- proto_ck_1/protoApp.js
+++ proto_ck_1/protoApp.js
@@ -1,7 +1,7 @@
 // Init svg
 var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+    width = document.getElementById('content').offsetWidth,
+    height = document.getElementById('content').offsetWidth * .75;
 
 var color = ''; // todo: make this an actual color or handle with css
 
@@ -18,7 +18,7 @@ var y = d3.scaleLinear().range([height,0]);
 
 // async ajax call baked into d3 to grab data
 // proto_ck_1a	gdelt_weblinks
-d3.json("/data/proto_ck_1b.json", function (error, graph) {
+d3.json("/data/proto_ck_1a.json", function (error, graph) {
  	if (error) throw error;
  	window.globalGraph = new GlobalGraph(graph);
 });
@@ -32,6 +32,11 @@ function GlobalGraph (graph) {
 	var self = this;
 
 	this.user = {};
+	this.graph = graph;
+
+	svg.on("click", function () {
+		updateNodes(self.graph.nodes);
+	});
 
 	// d3 selection containing all edge lines
 	this.link = svg.append("g") // todo: change this var name to edge?
@@ -43,22 +48,8 @@ function GlobalGraph (graph) {
 		// .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
 
 	// d3 selection containing all node circles
-	this.node = svg.append("g")
-		.attr("class", "nodes")
-		.selectAll("circle")
-		.data(graph.nodes)
-		.enter().append("circle")
-		.attr("r", function (d) {
-			if (d.count) return d.count;
-			return 5;
-		})
-		.attr("fill", function (d) {
-			return d.isActive ? "steelblue" : "black";
-		})
-		.call(d3.drag()
-			.on("start", dragStarted)
-			.on("drag", dragged)
-			.on("end", dragEnded));
+	updateNodes(self.graph.nodes)
+
 
 	// should enable browswer default tooltip on over
 	this.node.append("title")
@@ -72,6 +63,49 @@ function GlobalGraph (graph) {
 	// not sure I quite understand this yet...
 	simulation.force("link")
 		.links(graph.edges);
+
+
+	function updateNodes (nodes) {
+		var _nodes = (self.user && self.user.history) ? updateNodeData(nodes, self.user.history) : nodes;
+		console.log('updating nodes');
+		self.node = svg.append("g")
+			.attr("class", "nodes")
+			.selectAll("circle")
+			.data(_nodes)
+			.enter()
+			.append("circle")
+			.attr("r", function (d) {
+				if (d.count) return d.count;
+				return 5;
+			})
+			.attr("fill", function (d) {
+				return d.isActive ? "steelblue" : "black";
+			})
+			.call(d3.drag()
+				.on("start", dragStarted)
+				.on("drag", dragged)
+				.on("end", dragEnded));
+
+
+		// This seems so wrong.... whatever happened to Don't Repeat Yourself....?
+		self.node.merge(self.node)
+			.attr("r", function (d) {
+				if (d.count) return d.count;
+				return 5;
+			})
+			.attr("fill", function (d) {
+				return d.isActive ? "steelblue" : "black";
+			});
+
+		// This doesn't seem to do anything...
+		self.node.exit().remove();
+
+
+	}
+
+	function updateNodeData (gNodes, uNodes) {
+		return gNodes;
+	}
 
 	// call back function for simulation tick, re-renders all nodes and edges
 	function ticked () {
