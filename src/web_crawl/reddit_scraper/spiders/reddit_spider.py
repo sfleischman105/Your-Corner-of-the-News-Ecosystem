73dc8bfdedf14bd386edb2f6393d6ebc6bb9b829
Stephen Fleischman <stephenf@cs.washington.edu>
2017-04-28 02:33:54 -0700

    Added Comments to Web Scraper

--- src/web_crawl/reddit_scraper/spiders/reddit_spider.py
+++ src/web_crawl/reddit_scraper/spiders/reddit_spider.py
@@ -1,31 +1,46 @@
+# Utilizing Scrapy.py, runs through subreddits located inside subreddits.txt
+# exports raw HTML files into the ./sites folder
+#
+# Currently has the ability to go into deeper pages of reddit, up to a
+# custom amount
+#
+# In the future will be able to directly extract <div>s and their associated
+# information for stuff like: link, upvotes, etc.
+#
+
+
 # from bs4 import BeautifulSoup
 import scrapy
 from scrapy.contrib.linkextractors import LinkExtractor
 import re
 
 
-
 class RedditSpider(scrapy.Spider):
+    # Basic variables, name of the Spider + Sites allowed
     name = "reddit"
     allowed_domains = ["www.reddit.com"]
 
+    # write file for future additions concerning exporting data
     write_sites = open("subreddits_exp.txt","w")
 
-
+    # Function to start scraping, calls Parse in each site to examine HTML contents
     def start_requests(self):
         urls = subreddits_scraped()
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+    # Operations to be perfomed once HTML is received
     def parse(self, response):
+
+
         site_table = response.css('div.siteTable')
         posts = []
+
         # for div in site_table:
         #     if
         #
         #
         # for post in response.css('div.siteTable '):
-
 
         page = response.url.split("/")[-3]
         count = 0
@@ -48,7 +63,7 @@ class RedditSpider(scrapy.Spider):
             yield scrapy.Request(str(next_page), callback=self.parse)
 
 
-
+# Function to return list of URLS from designated subreddits from subreddits.txt
 def subreddits_scraped():
     base = "https://www.reddit.com/r/"
     top = "/top/?sort=top&t=all"
