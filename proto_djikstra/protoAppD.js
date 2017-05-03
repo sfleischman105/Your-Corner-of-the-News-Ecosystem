ea3f8ebc8321cab2eeeb50d7806a171c57fa4716
stephenf <stephenf@cs.washington.edu>
2017-05-03 01:30:30 -0700

    Dijkstras is mostly working. Code is pasta-sauce though (in the bad way)

--- proto_djikstra/protoAppD.js
+++ proto_djikstra/protoAppD.js
@@ -3,7 +3,7 @@
 
 var svg = d3.select("svg"),
     width = document.getElementById('graphContainer').offsetWidth,
-	height = document.getElementById('graphContainer').offsetWidth * .75;
+    height = document.getElementById('graphContainer').offsetWidth * .75;
 
 // svg.attr('width', width).attr('height', height);
 
@@ -11,15 +11,15 @@ var color = ''; // todo: make this an actual color or handle with css
 
 // simulation actually renders the graph and handles force animations
 var simulation = d3.forceSimulation()
-	.force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
     .force("charge", d3.forceManyBody().strength([-250])) // default strength -30)
     .force("center", d3.forceCenter(width / 2, height / 2));
 
 
 // async ajax call baked into d3 to grab data
 d3.json("reddit_data_test.json", function (error, graph) {
- 	if (error) throw error;
- 	window.globalGraph = new GlobalGraph(graph);
+    if (error) throw error;
+    window.globalGraph = new GlobalGraph(graph);
 });
 
 // GlobalGraph object to initialize and render the global news network graph
@@ -27,39 +27,42 @@ d3.json("reddit_data_test.json", function (error, graph) {
 // graph.nodes [] : array of node objects
 // graph.edges [] : array of edge objects
 function GlobalGraph (graph) {
-	var self = this;
+    var self = this;
 
-	this.user = {};
+    this.user = {};
 
-	graph.nodes.forEach( function(d) {
-		d.links = [];
-	});
+    graph.nodes.forEach( function(d) {
+        d.links = [];
+        d.distance = 0;
+        d.visited = false;
+        d.total_distance = 0;
+    });
 
-	// graph.edges.forEach( function(d) {
-     //    var src = d.source;
-     //    src.links.push(d);
-	// });
+    // graph.edges.forEach( function(d) {
+    //    var src = d.source;
+    //    src.links.push(d);
+    // });
 
-	// d3 selection containing all node circles
-	this.node = svg.append("g")
-		.attr("class", "node")
-		.selectAll(".node")
-		.data(graph.nodes)
-		.enter().append("circle")
-		.on("click", function() {
-			d3.select(this).attr("fill", "red")
+    // d3 selection containing all node circles
+    this.node = svg.append("g")
+        .attr("class", "node")
+        .selectAll(".node")
+        .data(graph.nodes)
+        .enter().append("circle")
+        // .on("click", function() {
+        // d3.select(this).attr("fill", "red")
+        // })
+        .attr("r", function (d) {
+            if (d.count) return d.count * 7;
+            return 8;
         })
-		.attr("r", function (d) {
-			if (d.count) return d.count * 7;
-			return 8;
-		})
-		.attr("fill", function (d) {
-			return d.isActive ? "steelblue" : "black";
-		})
-		.call(d3.drag()
-			.on("start", dragStarted)
-			.on("drag", dragged)
-			.on("end", dragEnded));
+        .attr("fill", function (d) {
+            return d.isActive ? "steelblue" : "black";
+        })
+        .call(d3.drag()
+            .on("start", dragStarted)
+            .on("drag", dragged)
+            .on("end", dragEnded));
 
 
     // d3 selection containing all edge lines
@@ -74,40 +77,40 @@ function GlobalGraph (graph) {
     // d3.selectAll(".link").call(update_node_src);
     //
     // function update_node_src() {
-		// var src = self.source;
-		// src.links.push(self);
+    // var src = self.source;
+    // src.links.push(self);
     // }
 
-	// should enable browswer default tooltip on over
-	this.node.append("title")
-		.text(function (d) { return d.id });
+    // should enable browswer default tooltip on over
+    this.node.append("title")
+        .text(function (d) { return d.id });
 
-	// simulation driving the animation via tick callback
-	simulation
-		.nodes(graph.nodes)
-		.on("tick", ticked);
+    // simulation driving the animation via tick callback
+    simulation
+        .nodes(graph.nodes)
+        .on("tick", ticked);
 
-	// not sure I quite understand this yet...
-	simulation.force("link")
-		.links(graph.edges);
+    // not sure I quite understand this yet...
+    simulation.force("link")
+        .links(graph.edges);
 
-	// call back function for simulation tick, re-renders all nodes and edges
-	function ticked () {
-		self.link
-			.attr("x1", function (d) { return d.source.x; })
-			.attr("y1", function (d) { return d.source.y; })
-			.attr("x2", function (d) { return d.target.x; })
-			.attr("y2", function (d) { return d.target.y; });
+    // call back function for simulation tick, re-renders all nodes and edges
+    function ticked () {
+        self.link
+            .attr("x1", function (d) { return d.source.x; })
+            .attr("y1", function (d) { return d.source.y; })
+            .attr("x2", function (d) { return d.target.x; })
+            .attr("y2", function (d) { return d.target.y; });
 
-		self.node
-			.attr("cx", function (d) { return d.x; })
-			.attr("cy", function (d) { return d.y; });
-	}
+        self.node
+            .attr("cx", function (d) { return d.x; })
+            .attr("cy", function (d) { return d.y; });
+    }
 
-	this.node.on("mouseover", function() {
-		d3.select(this)
-			.attr("r",10)
-	});
+    this.node.on("mouseover", function() {
+        d3.select(this)
+            .attr("r",10)
+    });
 
     this.node.on("mouseout", function() {
         d3.select(this)
@@ -120,83 +123,124 @@ function GlobalGraph (graph) {
     });
 
 
-    var dijkstra = d3.dijkstra()
-		.nodes(this.node)
-		.edges(this.edges);
-
 
     var color = d3.scaleLinear()
-        .domain([0, 1000, 2500])
+        .domain([0, 3, 5])
         .range(["green", "yellow", "red"]);
 
-    dijkstra.on("tick", function() {
-        node.style("fill", function(d) { return color(d.distance); });
-    });
+    var t = d3.transition().duration(2000);
 
-    dijkstra.on("end", function() {
-        console.log("Hello");
-    });
+    function tick() {
+        self.node.transition(t).style("fill", function(d) { return color(d.distance); });
+    }
+    // dijkstra.on("tick", function() {
+    //     self.node.style("fill", function(d) { return color(d.distance); });
+    // });
 
-    this.node.on("click", dijkstra.start);
+    function dijkstra(first) {
+        var unvisited = [];
+        graph.nodes.forEach(function (d) {
+            if (d != first) {
+                d.distance = Infinity;
+                unvisited.push(d);
+                d.visited = false;
+            }
+        });
+
+        var current = first;
+        current.distance = 0;
+        // current.total_distance = 0;
+        var done = false;
+
+        function stepi() {
+            current.visited = true;
+            // current.total_distance = 0;
+            current.links.forEach(function (link) {
+                var tar = link.target;
+                if (!tar.visited) {
+                    // var dist = current.distance + link.count;
+                    var dist = current.distance + 1;
+                    tar.distance = Math.min(dist, tar.distance);
+                }
+            });
+            if (unvisited.length == 0 || current.distance == Infinity) {
+                done = true;
+            }
+
+            unvisited.sort(function (a, b) {
+                return b.distance - a.distance
+            });
+            current = unvisited.pop();
+            tick();
+
+        }
+
+        if(!done) {
+            stepi();
+        }
+    }
 
 
+    this.node.on("click", dijkstra);
 }
+
+
 
 // drag start event handler on nodes
 function dragStarted (d) {
-	if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-	d.fx = d.x;
-	d.fy = d.y;
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
 }
 
 // drag event handler on nodes
 function dragged (d) {
-	d.fx = d3.event.x;
-	d.fy = d3.event.y;
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
 }
 
 // drag end event handler on nodes
 function dragEnded (d) {
-	if (!d3.event.active) simulation.alphaTarget(0);
-	d.fx = null;
-	d.fy = null;
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
 }
 
 
 // ProtoApp is the frontend app controllng the front-end and integrating D3 and user interactions
 function ProtoApp (options) {
-	options = arguments[0] || null;
+    options = arguments[0] || null;
 
-	var self = this;
-	this.userData = {};
+    var self = this;
+    this.userData = {};
 
-	this.initialize = function () {
-		this.addEventListeners();
-	},
+    this.initialize = function () {
+        this.addEventListeners();
+    },
 
-	this.addEventListeners = function () {
-		$('#refreshGraph').on('click', this.onRefreshGraph);
-		$('#addStubData').on('click', this.onAddStubData);
-	},
+        this.addEventListeners = function () {
+            $('#refreshGraph').on('click', this.onRefreshGraph);
+            $('#addStubData').on('click', this.onAddStubData);
+        },
 
-	this.onRefreshGraph = function (e) {
-		// tell D3 to do it's thing
-	},
+        this.onRefreshGraph = function (e) {
+            // tell D3 to do it's thing
+        },
 
-	this.onAddStubData = function (e) {
-		var filenameString = $('#stubDataSelect').find('option:selected').attr('value');
-		var filePath = '/data/' + filenameString + '.json';
+        this.onAddStubData = function (e) {
+            var filenameString = $('#stubDataSelect').find('option:selected').attr('value');
+            var filePath = '/data/' + filenameString + '.json';
 
-		$.get(filePath);
-	}
+            $.get(filePath);
+        }
 
-	this.handleStubData = function (data) {
-		self.userData = data;
-		// console.log(data);
-		// do things with stub data
-	}
+    this.handleStubData = function (data) {
+        self.userData = data;
+        // console.log(data);
+        // do things with stub data
+    }
 
-	this.initialize();
+    this.initialize();
 };
 
 var protoApp = new ProtoApp();
